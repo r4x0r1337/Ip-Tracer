97561abc096f117acbe88e4e84756a587d30cfe3
BlackyGuy <99073650+Blackyguy@users.noreply.github.com>
2022-02-22 23:17:29 +0800

    Update iptracer.py

--- iptracer.py
+++ iptracer.py
@@ -3,12 +3,12 @@ from urllib import request
 
 print("\n== IP Tracker - Xmall75 ==\n")
 url = "http://ip-api.com/json/"
-ip = input("Masukkan IP Address : ")
+ip = input("Input the IP Address : ")
 request = request.urlopen(url + ip)
 data_json = json.loads(request.read())
 
 if data_json['status'] == "success":
-	print(f"IP Address: {data_json['query']}")
+	print(f"IP : {data_json['query']}")
 	print(f"Country : {data_json['country']}")
 	print(f"Region : {data_json['regionName']}")
 	print(f"City : {data_json['city']}")
@@ -16,4 +16,4 @@ if data_json['status'] == "success":
 	print(f"Longitude : {data_json['lon']}")
 	print(f"ISP : {data_json['isp']}")
 else:
-	print("Gagal mencari maklumat IP Adress")
+	print("Failed to find the IP informations.")
